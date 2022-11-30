973bea23cf678693d01d58c3215926051c8a248f
chaimdavid <87780938+chaimdavid@users.noreply.github.com>
2022-11-30 15:01:58 +0200

    pub

--- Lin_Reg_ML_Model.py
+++ Lin_Reg_ML_Model.py
@@ -6,8 +6,7 @@ from pyspark.ml.linalg import Vectors
 from pyspark.ml.feature import VectorAssembler
 spark=SparkSession.builder.appName('cruise').getOrCreate()
 #Importing the data into a Spark dataframe
-#data=spark.read.csv('cruise_ship_info.csv',inferSchema=True,header=True)
-data=spark.read.csv(r'C:\Users\h_dav\Dropbox\Workearly\6. Spark and Python for Big Data with PySpark\Python-and-Spark-for-Big-Data-master\Spark_for_Machine_Learning\Linear_Regression\cruise_ship_info.csv',inferSchema=True,header=True)
+data=spark.read.csv('cruise_ship_info.csv',inferSchema=True,header=True)
 #Indexing the contents of a column that are strings so that it can be used as a feature from the model
 indexer=StringIndexer(inputCol='Cruise_line',outputCol='lineIndex')
 indexed=indexer.fit(data).transform(data)
@@ -17,14 +16,19 @@ assembler=VectorAssembler(inputCols=['Age', 'Tonnage', 'passengers', 'length', '
 output=assembler.transform(indexed)
 output.select('features').show()
 final_data=output.select('features','crew')
+#Splitting the original data into two sub-dataframes. A training one with the 70% of the original data and a test one with the remaining 30%
 train_data,test_data=final_data.randomSplit([0.7,0.3])
+#Creation of the model
 lr = LinearRegression(labelCol='crew')
 lrModel = lr.fit(train_data,)
 print("Coefficients: {} Intercept: {}".format(lrModel.coefficients,lrModel.intercept))
+#Evaluation of the model by making use of the untouched test data
 test_results = lrModel.evaluate(test_data)
 test_results.residuals.show()
+#Creation of the model's predictions
 unlabeled_data = test_data.select('features')
 predictions = lrModel.transform(unlabeled_data)
 predictions.show()
+#Printing of two statistical evaluation measures
 print("RMSE: {}".format(test_results.rootMeanSquaredError))
 print("MSE: {}".format(test_results.meanSquaredError))
